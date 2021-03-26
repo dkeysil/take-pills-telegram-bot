7aef96deedef731b0f2051969223c6e39a548beb
d.kisel <d.kisel@mcart.ru>
2021-03-27 01:28:55 +0300

    add logging grafana

--- tgbot/bot/__init__.py
+++ tgbot/bot/__init__.py
@@ -2,11 +2,19 @@ from aiogram import Bot, Dispatcher, types
 import os
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
 import uvloop
+from aiogram_logging import Logger, InfluxSender
+from aiogram.dispatcher.middlewares import BaseMiddleware
 
 
 API_TOKEN = os.environ['BOT_API_KEY']
 
 bot = Bot(token=API_TOKEN, parse_mode='html')
+
+sender = InfluxSender(host='172.17.0.1',
+                      db='telegram',
+                      username='user',
+                      password='password')
+log = Logger(sender_class=sender)
 
 storage = MongoStorage(host=os.environ['MONGODB_HOSTNAME'],
                        port=os.environ['MONGODB_PORT'],
@@ -15,6 +23,17 @@ storage = MongoStorage(host=os.environ['MONGODB_HOSTNAME'],
 
 dp = Dispatcher(bot, storage=storage)
 
+
+class StatMiddleware(BaseMiddleware):
+
+    def __init__(self):
+        super(StatMiddleware, self).__init__()
+
+    async def on_process_message(self, message: types.Message, data: dict):
+        await log.write_logs(self._manager.bot.id, message, parse_text=True)
+
+
+dp.middleware.setup(StatMiddleware())
 uvloop.install()
 
 
